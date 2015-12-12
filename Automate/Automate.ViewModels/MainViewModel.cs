858e254ff9729ed9f7e6a57abc76db0c25bf6106
Alexander Bleissem <alexander@bleissem.com>
2015-12-12 01:59:06 +0100

    #1 - can be compiled

--- Automate/Automate.ViewModels/MainViewModel.cs
+++ Automate/Automate.ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Automate.Wrapper;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,20 +12,13 @@ namespace Automate.ViewModels
         public MainViewModel()
         {
             this.MousMove = "Hello Automate";
-            m_Hook = new Hook();
-            m_Hook.RegisterMouse((msg) =>
-            {
-                this.MousMove = msg;
-            });
-        }
+       }
 
 
         ~MainViewModel()
         {
-            m_Hook.Dispose();
         }
 
-        private Hook m_Hook;
 
         private string _MouseMove;
         public string MousMove
