1de078d33752d1650543fcb73493240f04e7c4ae
Alexander Bleissem <alexander@bleissem.com>
2015-12-12 01:47:49 +0100

    #1 - added missing files

---
+++ Automate/Automate.ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Automate.Wrapper;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automate.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel()
+        {
+            this.MousMove = "Hello Automate";
+            m_Hook = new Hook();
+            m_Hook.RegisterMouse((msg) =>
+            {
+                this.MousMove = msg;
+            });
+        }
+
+
+        ~MainViewModel()
+        {
+            m_Hook.Dispose();
+        }
+
+        private Hook m_Hook;
+
+        private string _MouseMove;
+        public string MousMove
+        {
+            get
+            {
+                return _MouseMove;
+            }
+            set
+            {
+                _MouseMove = value;
+                base.OnPropertyChanged(() => MousMove);
+            }
+        }
+    }
+}
